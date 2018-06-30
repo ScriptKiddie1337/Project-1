373c0a38db75e2cfc431ed8d8bd29a5b1dff8f72
bjl72 <bjlaney@alum.rpi.edu>
2018-06-30 12:49:43 -0700

    update with locations

---
+++ assets/js/getLocations.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+
+    var lat = "";
+    var long = "";
+    var location =""
+    var searchRadius ="1500"
+    var searchType = "restaurant"
+
+    $.ajaxPrefilter(function(options) {
+        if (options.crossDomain && $.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+
+
+    findLocationByBrowser();
+    // Hardcoded 5000m pizza search
+    var key = "AIzaSyAanF7HzrZs0nv_lhvjDcr2vvjtmbyJYAQ";
+
+
+    $("#testButton").on("click", function () {
+        findLocationByBrowser();
+    });
+
+    $("#testLocations").on("click", function () {
+        getLocations();
+    });
+
+    function findLocationByBrowser() {
+        navigator.geolocation.getCurrentPosition(getLocationSuccess, getLocationError);
+    }
+
+    function getLocationSuccess(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        location = lat + "," + long;
+
+        console.log(location);
+
+        /*   do_something(position.coords.latitude, position.coords.longitude); */
+
+      /*   var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat.toString()},${long.toString()}&key=${key}`; */
+        var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.766710,-117.226453&key=AIzaSyAanF7HzrZs0nv_lhvjDcr2vvjtmbyJYAQ`;
+
+       /*  document.getElementById("#map"). */
+        getAjaxLocation(url)  
+    }
+
+    function getLocationError(err) {
+        console.log('Error geolocating', err);
+
+         var url= `https://maps.googleapis.com/maps/api/place/textsearch/json?location=32.766710,-117.226453&radius=${searchRadius}&type=${searchType}&rankby=distance&keyword=pizza&${key}&formatted_address`;
+
+         getAjaxLocation(url)
+    }
+
+    function getAjaxLocation(url){
+
+        $.ajax({
+            url: url,
+            request: "GET",
+            dataType: 'json',
+        }).then(function (response) {
+            console.log(response.results);
+        });
+    }
+
+    var map;
+    var service;
+
+    function getLocations() {
+        var mapCenter = new google.maps.LatLng(lat, long);
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: mapCenter,
+            zoom: 1
+        });
+
+        var request = {
+            location: mapCenter,
+            radius: '1500',
+            type: ['restaurant']
+        };
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+    }
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                createMarker(results[i]);
+                console.log(place);
+            }
+        }
+    }
+
+});
--- assets/js/nearbySearch.js
+++ assets/js/nearbySearch.js
@@ -1,11 +1,11 @@
 // Hardcoded 5000m pizza search
 
-let key = "key=AIzaSyAqx5_ZLRfWwMNRyxFNuvdvkSiD3tSvfxM"
+let key = "key=AIzaSyAJjBoiRwsuo2RoC88RvMD4yWfEnyN0Hl0";
 
 $.ajax({
     url: `https://maps.googleapis.com/maps/api/place/textsearch/json?location=32.766710,-117.226453&radius=5000&type=restaurant&keyword=pizzae&${key}&formatted_address`,
     request: "GET",
     dataType: 'jsonp',
 }).then(function (response) {
-    console.log(response.results)
+    console.log(response.results);
 })
